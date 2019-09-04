74e632b05a1b2e8b4dd2a36135307262d96ab0f7
Audeta <audeta35@gmail.com>
2019-09-04 19:54:12 +0700

    minor fix

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -128,11 +128,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="../../index3.html" class="brand-link">
-      <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}"
-           alt="AdminLTE Logo"
-           class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">{{config('app.name')}}</span>
+      <img src="{{asset('admin/dist/img/AdminLTELogo.png')}}" alt="{{config('app.name')}} Logo" class="brand-image img-circle elevation-3">
+      <span class="brand-text font-weight-light"> {{config('app.name')}}</span>
     </a>
 
     <!-- Sidebar -->
@@ -143,7 +140,7 @@
           <img src="{{asset('admin/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">{{ Auth::user()->email }}</a>
         </div>
       </div>
 
